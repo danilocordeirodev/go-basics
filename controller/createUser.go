9b1c7dcf6a559232c20345ad91f6cf95df694519
Danilo Cordeiro <danilocordeiro.ti@gmail.com>
2023-07-25 09:24:53 -0300

    add log

--- controller/createUser.go
+++ controller/createUser.go
@@ -2,9 +2,12 @@ package controller
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 
-	"github.com/danilocordeirodev/go-basics/config/rest_err"
+	"github.com/danilocordeirodev/go-basics/config/validation"
 	"github.com/danilocordeirodev/go-basics/controller/dto/request"
+	"github.com/danilocordeirodev/go-basics/controller/dto/response"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,13 +16,22 @@ func CreateUser(c *gin.Context) {
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		restErr := rest_err.NewBadRequestError(
-			fmt.Sprintf("There are some incorrect fields, error=%s", err.Error()))
+		log.Printf("Error trying to create user: %v", err.Error())
+		errRest := validation.ValidateUserError(err)
 
-		c.JSON(restErr.Code, restErr)
+		c.JSON(errRest.Code, errRest)
 		return
 	}
 
 	fmt.Println(userRequest)
+
+	response := response.UserResponse{
+		ID: "test",
+		Email: userRequest.Email,
+		Name: userRequest.Name,
+		Age: userRequest.Age,
+	}
+
+	c.JSON(http.StatusOK, response)
 
 }
