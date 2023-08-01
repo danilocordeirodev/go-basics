6867a34f8f9abbd199a32ec798b528b941117671
Danilo Cordeiro <danilocordeiro.ti@gmail.com>
2023-08-01 19:00:08 -0300

    refactoring domain

--- model/user_domain.go
+++ model/user_domain.go
@@ -1,35 +1,5 @@
 package model
 
-import (
-	"crypto/md5"
-	"encoding/hex"
-)
-
-type UserDomainInterface interface {
-	GetEmail() string
-	GetAge() int8
-	GetPassword() string
-	GetName() string
-	GetID() string
-	SetID(string)
-	EncryptPassword()
-}
-
-func NewUserDomain(
-	email, password, name string,
-	age int8,
-) UserDomainInterface {
-	return &userDomain{
-		email: email, 
-		password: password,
-		name: name, 
-		age: age,
-	}
-}
-
-func (ud *userDomain) SetID(id string) {
-	ud.id = id
-}
 
 type userDomain struct {
 	id string
@@ -37,6 +7,10 @@ type userDomain struct {
 	password string
 	name string
 	age int8
+}
+
+func (ud *userDomain) SetID(id string) {
+	ud.id = id
 }
 
 func (ud *userDomain) GetID() string{
@@ -54,12 +28,5 @@ func (ud *userDomain) GetName() string{
 }
 func (ud *userDomain) GetAge() int8{
 	return ud.age
-}
-
-func (ud *userDomain) EncryptPassword() {
-	hash := md5.New()
-	defer hash.Reset()
-	hash.Write([]byte(ud.password))
-	ud.password = hex.EncodeToString(hash.Sum(nil))
 }
 
